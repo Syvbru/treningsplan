8cd0df5797dbf7463e5504dd291ccfdd05e67a89
Syver Brustad <sybru6330@oslomet.no>
2026-04-27 20:46:47 +0200

    Fix environment variable declaration for FELLES_OKTER_SHEET_URL
    Remove redundant declaration of FELLES_OKTER_SHEET_URL.

--- src/routes/api/felles-okter-url/+server.js
+++ src/routes/api/felles-okter-url/+server.js
@@ -1,6 +1,5 @@
 import { json } from '@sveltejs/kit';
-
-const FELLES_OKTER_SHEET_URL = process.env.FELLES_OKTER_SHEET_URL;
+import { FELLES_OKTER_SHEET_URL } from '$env/static/private';
 
 if (!FELLES_OKTER_SHEET_URL) {
     throw new Error('FELLES_OKTER_SHEET_URL environment variable is not set');
